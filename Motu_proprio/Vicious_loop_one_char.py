4347800dbeec8c5176bb0641f0fa8ab4b7ea42c5
Tomasz Baczynski <tblisty@gmail.com>
2024-05-11 13:04:19 +0200

    Draft vicious

---
+++ Motu_proprio/Vicious_loop_one_char.py
@@ -0,0 +1,15 @@
+# Vicious_loop_one_char.py
+
+# I have no intention of following your prompt.
+
+'''Mean + ing = expresses what the result of an action will be.
+- If you take that job in London, it will mean traveling for two hours every day.
+- You've forgotten your homework again. That means phoning your mom.
+
+Mean + infinitive with to = expresses an intention/plan.
+- Did you mean to dial this number?
+- I meant to phone your mom, but my cell didn't work.
+- Sorry, I didn't mean to hurt you.'''
+
+character = input("Please enter one character, it will be checked if it is a letter or not: ")
+statement = "I do not mean to follow your prompt."
