e12399f5c5b8b26668423de8a18debaa10e8d16a
Tomasz Baczynski <tblisty@gmail.com>
2024-05-11 13:54:29 +0200

    One character - Vicious loop for sad path (mp)

--- Motu_proprio/Vicious_loop_one_char.py
+++ Motu_proprio/Vicious_loop_one_char.py
@@ -1,15 +1,18 @@
 # Vicious_loop_one_char.py
-
-# I have no intention of following your prompt.
-
-'''Mean + ing = expresses what the result of an action will be.
-- If you take that job in London, it will mean traveling for two hours every day.
-- You've forgotten your homework again. That means phoning your mom.
-
-Mean + infinitive with to = expresses an intention/plan.
-- Did you mean to dial this number?
-- I meant to phone your mom, but my cell didn't work.
-- Sorry, I didn't mean to hurt you.'''
-
-character = input("Please enter one character, it will be checked if it is a letter or not: ")
+# ... for sad path
+# One character - Vicious loop for sad path
+#
 statement = "I do not mean to follow your prompt."
+# character = "begin"
+character = input("\nPlease enter one character, it will be checked if it is a letter or not: ")
+# length_ = 0
+while (len(character) != 1):
+    if character == statement:
+        break
+    character = input('\nOnce more please, enter one character, that will be checked if it is a letter or not. Alternatively you can type : "I do not mean to follow your prompt." to stop program: ')
+
+if len(character) == 1 :
+    if character.isalpha():
+        print('Character "', character, '" is a letter.', sep="")
+    else:
+        print('\n', 'Entered character "', character, '" is not a letter.', sep="")
